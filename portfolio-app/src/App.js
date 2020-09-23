4c9a8851c97e0e31896181b24fb92dea53852e4b
Vishav Bangotra <vishav.bangotra@gmail.com>
2020-09-23 17:34:55 +0530

    Created React App

---
+++ portfolio-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Navbar from './Components/Navbar.js'
+import Test from './Components/Test.js'
+
+function App() {
+  return (
+    <div className="App">
+    <Navbar />
+    <Test />
+    </div>
+  );
+}
+
+export default App;
